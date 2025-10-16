591d71a0ef2ff6ead57bc451237db03584b21806
thphuongtz <gggggyyyo580@gmail.com>
2025-10-16 21:46:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from FAISS import build_faiss_index
 import matplotlib.pyplot as plt
 
 # --- Cấu hình ---
-DB_PATH = "data/known_faces"
+DB_PATH = "data/database"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # --- Khởi tạo MTCNN và ResNet ---
